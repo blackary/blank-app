349186cfe4b2f8c4c6854c1e57745df0dfd3c7da
Zachary Blackwood <zachary@streamlit.io>
2024-09-17 01:21:51 +0000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,13 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("Hi, class")
+
+st.write("""
+This is a very long line of text
+         
+It has lots of stuff
+""")
+
+st.audio("purr.mp3")
