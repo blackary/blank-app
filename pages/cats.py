56ba3b9d5fd91108c7eb37dc063bd944bc627b4c
Zachary Blackwood <zachary@streamlit.io>
2024-10-29 00:39:36 +0000

    hi

--- pages/cats.py
+++ pages/cats.py
@@ -2,10 +2,15 @@
 import streamlit as st
 import requests
 
-number = st.slider("Pick a pokemon", 1, 122)
-response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{number}/")
+def get_pokemon(num):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{number}/")
 
-st.image(response.json()["sprites"]["other"]["showdown"]["front_default"])
+    st.image(response.json()["sprites"]["other"]["showdown"]["front_default"], width=200)
+
+number = st.slider("Pick a pokemon", 1, 122)
+
+get_pokemon(number)
+
 
 # st.write(pyjokes.get_joke())
 
