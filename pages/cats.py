9f21c02f1f7f24422a469330411131ae3e4adc19
Zachary Blackwood <zachary@streamlit.io>
2024-10-29 00:42:03 +0000

    both

--- pages/cats.py
+++ pages/cats.py
@@ -4,8 +4,11 @@ import requests
 
 def get_pokemon(num):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{number}/")
+    data = response.json()
+    st.title(data["name"])
 
-    st.image(response.json()["sprites"]["other"]["showdown"]["front_default"], width=200)
+    st.image(data["sprites"]["other"]["showdown"]["front_shiny"], width=200)
+    st.image(data["sprites"]["other"]["showdown"]["back_shiny"], width=200)
 
 number = st.slider("Pick a pokemon", 1, 122)
 
